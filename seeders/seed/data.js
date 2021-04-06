8f5b38f805d9f49425302f01ab474afc7fcfff6f
Fan5 <openconvenience@gmail.com>
2021-04-06 16:59:50 +0800

    add id to seed data

--- seeders/seed/data.js
+++ seeders/seed/data.js
@@ -1,24 +1,25 @@
 module.exports = {
   categories: [
-    { name: '狗兒飼料' },
-    { name: '狗狗玩具' },
-    { name: '外出用品' },
-    { name: '狗狗零食' },
-    { name: '寵物保健' }
+    { id: 1, name: '狗兒飼料' },
+    { id: 2, name: '狗狗玩具' },
+    { id: 3, name: '外出用品' },
+    { id: 4, name: '狗狗零食' },
+    { id: 5, name: '寵物保健' }
   ],
   brands: [
-    { name: '渴望 Orijen' },
-    { name: 'Kong' },
-    { name: 'Pestro 沛德奧' },
-    { name: '自然牧場Natural Farm' },
-    { name: 'IN Plus' },
-    { name: '原點 Instinct' },
-    { name: 'AFP' },
-    { name: '柏萊富 Blackwood' },
-    { name: '燒肉工房' }
+    { id: 1, name: '渴望 Orijen' },
+    { id: 2, name: 'Kong' },
+    { id: 3, name: 'Pestro 沛德奧' },
+    { id: 4, name: '自然牧場Natural Farm' },
+    { id: 5, name: 'IN Plus' },
+    { id: 6, name: '原點 Instinct' },
+    { id: 7, name: 'AFP' },
+    { id: 8, name: '柏萊富 Blackwood' },
+    { id: 9, name: '燒肉工房' }
   ],
   products: [
     {
+      id: 1,
       name: '挑嘴幼犬 野牧鮮雞配方 2公斤 (狗飼料)',
       description: '渴望幼犬配方，專為１歲以下的愛狗精心調配。符合幼犬在大自然下的飲食方式，適合成長中幼犬及長大的年輕犬食用。有別於其他一般狗飼料，渴望犬飼料含有高蛋白，低碳水化合物且不含糧食，採用了80%人類食用高等級的營養肉類、20%新鮮蔬果、0%穀物。無論任何品種的幼犬，渴望都能幫助提升愛犬的免疫力與健康。豐富的雞肉蛋白質，符合幼犬成長所需的營養。另外添加了天然抗氧化劑和植物化學物質和天然DHA及EPA，幫助形成細胞膜和保護皮毛。本產品絕不含人工防腐劑最佳的嗜口性100 %滿意保證 。',
       price: 1800,
@@ -27,6 +28,7 @@ module.exports = {
       BrandId: 1,
     },
     {
+      id: 2,
       name: 'KONG 寵物訓練智力玩具',
       description: 'KONG Wobbler 能像陀螺一樣旋轉，如同不倒翁般不會倒下。有一個零食出口，食物會在旋轉中隨機驚喜掉出，上下盤可打開，可輕鬆補充食物。',
       price: 400,
@@ -35,6 +37,7 @@ module.exports = {
       BrandId: 2
     },
     {
+      id: 3,
       name: '【PETTIO 沛德奧】415P方舟系列寵物推車-深海藍',
       description: '可當推車/居家窩/寵物車用，20公斤的分離式寵物推車，帆蓋前後採YKK等級大齒拉',
       price: 4980,
@@ -43,6 +46,7 @@ module.exports = {
       BrandId: 3
     },
     {
+      id: 4,
       name: '鹿氣管120克 (狗零食)',
       description: '採用100% 紐西蘭天然鹿肉烘乾製成完整保留鹿肉的原汁原味高蛋白低脂肪，適合想要尋找低敏肉類的狗狗原汁原味 天然健康',
       price: 280,
@@ -51,6 +55,7 @@ module.exports = {
       BrandId: 4
     },
     {
+      id: 5,
       name: '超濃縮卵磷脂犬用 (中) (狗保健用品)',
       description: '使用獨家低溫冷壓技術，減少高溫處理的過程，生產出含有豐富營養素的可口軟顆粒。一顆卵磷脂中含有豐富礦物質及維他命，排除易敏感成分- 大豆、玉米、小麥、壓製成美味可口的軟顆粒，讓毛孩安心享用IN - Plus的完美配方。',
       price: 1350,
@@ -59,6 +64,7 @@ module.exports = {
       BrandId: 5
     },
     {
+      id: 6,
       name: '全犬配方 雞肉凍乾 1.8公斤 (狗飼料)',
       description: 'WDJ連年推薦Recommended by The Whole Dog Journal',
       price: 1460,
@@ -67,6 +73,7 @@ module.exports = {
       BrandId: 6
     },
     {
+      id: 7,
       name: '夏日系列 沙灘拖鞋(狗玩具)',
       description: '陪伴，訓練，安撫， 磨牙',
       price: 225,
@@ -75,6 +82,7 @@ module.exports = {
       BrandId: 7
     },
     {
+      id: 8,
       name: '蔬果系列 鳳梨(狗玩具)',
       description: '陪伴，訓練，安撫， 磨牙',
       price: 299,
@@ -83,6 +91,7 @@ module.exports = {
       BrandId: 7
     },
     {
+      id: 9,
       name: '功能性全齡護膚亮毛配方(羊肉+糙米) 5磅 (狗飼料)',
       description: '全齡階段全犬種',
       price: 782,
@@ -91,6 +100,7 @@ module.exports = {
       BrandId: 8
     },
     {
+      id: 10,
       name: '特調全齡犬配方(羊肉+糙米+雞肉) 15磅 (狗飼料)',
       description: '全天然優質羊肉，有效防食物過敏',
       price: 1600,
@@ -99,6 +109,7 @@ module.exports = {
       BrandId: 8
     },
     {
+      id: 11,
       name: '美味玩賞狗狗零食大包裝(兩袋入) 200克【蜜汁香醇雞腿片】(1包)',
       description: '自然風，低脂肪，低鹽分，新鮮雞肉。 ',
       price: 199,
@@ -109,6 +120,7 @@ module.exports = {
   ],
   users: [
     {
+      id: 1,
       name: 'root',
       email: 'root@seed.com',
       phone: '0970000000',
@@ -116,6 +128,7 @@ module.exports = {
       role: 'admin'
     },
     {
+      id: 2,
       name: 'user1',
       email: 'user1@seed.com',
       phone: '0970000001',
@@ -123,6 +136,7 @@ module.exports = {
       role: 'user'
     },
     {
+      id: 3,
       name: 'user2',
       email: 'user2@seed.com',
       phone: '0970000002',
@@ -130,6 +144,7 @@ module.exports = {
       role: 'user'
     },
     {
+      id: 4,
       name: 'user3',
       email: 'user3@seed.com',
       phone: '0970000003',
